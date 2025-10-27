a0aa9cd9cce330f015cb3065f45e4425f111a4e1
elitalpa <eli@eliapps.com>
2025-10-28 00:13:10 +0100

    feat: adding dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // import { Suspense } from "react";
 import { useTranslation } from "react-i18next";
-import { LanguageSwitcher } from "./components/language-switcher";
+import { LanguageSwitcher } from "@/components/language-switcher";
+import { ModeToggle } from "@/components/mode-toggle";
 
 function App() {
   const { t } = useTranslation();
 
   return (
     <main className="flex flex-col items-center justify-center h-screen relative">
-      <div className="absolute top-4 right-4">
+      <div className="absolute top-4 right-4 flex gap-2">
+        <ModeToggle />
         <LanguageSwitcher />
       </div>
       <h1 className="text-4xl font-bold">{t("app.title")}</h1>
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
+import { ThemeProvider } from "@/components/theme-provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="system" storageKey="verseprojector-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
