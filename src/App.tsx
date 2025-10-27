6b104611c2616d77e9164f56bbd4250c64f73d93
elitalpa <eli@eliapps.com>
2025-10-27 02:00:35 +0100

    feat: description + layout fix + seo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 function App() {
   return (
-    <html>
-      <body>
-        <main className="flex flex-col items-center justify-center h-screen">
-          <h1 className="text-4xl font-bold">VerseProjector</h1>
-        </main>
-      </body>
-    </html>
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">VerseProjector</h1>
+      <p className="text-muted-foreground mt-2">
+        Display verses on a projector or a screen.
+      </p>
+    </main>
   );
 }
 
