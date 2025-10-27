2dd08e2fed85bc7c7357eadc23b7dca6f572ebda
elitalpa <eli@eliapps.com>
2025-10-27 01:49:15 +0100

    build: adding files for setup with "React + TypeScript + Vite" + electron-builder + shadcn ui components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <html>
+      <body>
+        <main className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold">VerseProjector</h1>
+        </main>
+      </body>
+    </html>
+  );
+}
+
+export default App;
---
+++ vite.config.web.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist-web",
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
